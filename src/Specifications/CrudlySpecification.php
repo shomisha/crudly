05b3f2ece17ba7f153c22831066c2b1ec600b5df
Miša Ković <misa95kovic@gmail.com>
2020-12-28 00:43:10 +0100

    feat(crudly): implement forseeable specifications

---
+++ src/Specifications/CrudlySpecification.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Shomisha\Crudly\Specifications;
+
+use Illuminate\Support\Collection;
+use Shomisha\Crudly\Abstracts\Specification;
+use Shomisha\Crudly\Data\ModelName;
+
+class CrudlySpecification extends Specification
+{
+    const KEY_MODEL = 'model';
+
+    const KEY_PROPERTIES = 'properties';
+
+    const KEY_HAS_WEB = 'has_web';
+
+    const KEY_HAS_API = 'has_api';
+
+    const KEY_PRIMARY = 'primary';
+
+    const KEY_ACTUAL_PRIMARY = 'actual_primary_key';
+
+    public function __construct(ModelName $model, array $data = [])
+    {
+        parent::__construct($data);
+
+        $this->set(self::KEY_MODEL, $model);
+
+        $this->parseProperties();
+        $this->parsePrimary();
+    }
+
+    public function getModel(): ModelName
+    {
+        return $this->extract(self::KEY_MODEL);
+    }
+
+    /** @return \Illuminate\Support\Collection|\Shomisha\Crudly\Specifications\ModelPropertySpecification[] */
+    public function getProperties(): Collection
+    {
+        return $this->extract(self::KEY_PROPERTIES);
+    }
+
+    public function hasWeb(): bool
+    {
+        return $this->extract(self::KEY_HAS_WEB);
+    }
+
+    public function hasApi(): bool
+    {
+        return $this->extract(self::KEY_HAS_API);
+    }
+
+    private function parseProperties(): void
+    {
+        $properties = collect($this->extract(self::KEY_PROPERTIES))
+            ->keyBy('name')
+            ->map(function (array $propertyData) {
+                return new ModelPropertySpecification($propertyData);
+            });
+
+        $this->set(self::KEY_PROPERTIES, $properties);
+    }
+
+    private function parsePrimary(): void
+    {
+        if ($actualPrimaryProperty = $this->extract(self::KEY_ACTUAL_PRIMARY)) {
+            $primaryKey = $this->getProperties()->first(function (ModelPropertySpecification $property) use ($actualPrimaryProperty) {
+                return $property->getName() === $actualPrimaryProperty;
+            });
+        } else {
+            $primaryKey = $this->getProperties()->first(function (ModelPropertySpecification $property) {
+                return $property->isPrimaryKey();
+            });
+        }
+
+        $this->set(self::KEY_PRIMARY, $primaryKey);
+    }
+}
---
+++ src/Specifications/ModelPropertySpecification.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Shomisha\Crudly\Specifications;
+
+use Shomisha\Crudly\Abstracts\Specification;
+use Shomisha\Crudly\Enums\ForeignKeyAction;
+use Shomisha\Crudly\Enums\ModelPropertyType;
+
+class ModelPropertySpecification extends Specification
+{
+    private const
+        KEY_NAME = 'name',
+        KEY_TYPE = 'type',
+        KEY_AUTOINCREMENT = 'is_autoincrement',
+        KEY_UNSIGNED = 'is_unsigned',
+        KEY_UNIQUE = 'is_unique',
+        KEY_NULLABLE = 'is_nullable',
+        KEY_FOREIGN_KEY = 'is_foreign_key',
+        KEY_PRIMARY_KEY = 'is_primary',
+        KEY_FOREIGN_KEY_TABLE = 'foreign_key_target.table',
+        KEY_FOREIGN_KEY_FIELD = 'foreign_key_target.field',
+        KEY_FOREIGN_KEY_ON_DELETE = 'foreign_key_target.on_delete',
+        KEY_FOREIGN_KEY_ON_UPDATE = 'foreign_key_target.on_update';
+
+    public function getName(): string
+    {
+        return $this->extract(self::KEY_NAME);
+    }
+
+    public function getType(): ModelPropertyType
+    {
+        return ModelPropertyType::fromString($this->extract(self::KEY_TYPE));
+    }
+
+    public function isAutoincrement(): bool
+    {
+        return $this->extract(self::KEY_AUTOINCREMENT);
+    }
+
+    public function isUnsigned(): bool
+    {
+        return $this->extract(self::KEY_UNSIGNED);
+    }
+
+    public function isUnique(): bool
+    {
+        return $this->extract(self::KEY_UNIQUE);
+    }
+
+    public function isNullable(): bool
+    {
+        return $this->extract(self::KEY_NULLABLE);
+    }
+
+    public function isForeignKey(): bool
+    {
+        return $this->extract(self::KEY_FOREIGN_KEY);
+    }
+
+    public function isPrimaryKey(): bool
+    {
+        return $this->extract(self::KEY_PRIMARY_KEY) ?? false;
+    }
+
+    public function getForeignKeyTable(): ?string
+    {
+        return $this->extract(self::KEY_FOREIGN_KEY_TABLE);
+    }
+
+    public function getForeignKeyField(): ?string
+    {
+        return $this->extract(self::KEY_FOREIGN_KEY_FIELD);
+    }
+
+    public function getForeignKeyOnDelete(): ?ForeignKeyAction
+    {
+        return $this->extract(self::KEY_FOREIGN_KEY_ON_DELETE);
+    }
+
+    public function getForeignKeyOnUpdate(): ?ForeignKeyAction
+    {
+        return $this->extract(self::KEY_FOREIGN_KEY_ON_UPDATE);
+    }
+}
