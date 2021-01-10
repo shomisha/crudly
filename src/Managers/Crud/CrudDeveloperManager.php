b62427fc69ea17de9a562704f64f60d0cd3286fa
Miša Ković <misa95kovic@gmail.com>
2021-01-10 13:36:33 +0100

    feat(crud): extract the CrudDeveloperManager, extend it using CrudMethodDeveloperManager

---
+++ src/Managers/Crud/CrudDeveloperManager.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Shomisha\Crudly\Managers\Crud;
+
+use Shomisha\Crudly\Developers\Crud\PartialDevelopers\ImplicitBindArgumentsDeveloper;
+use Shomisha\Crudly\Developers\Crud\PartialDevelopers\InstantiateDeveloper;
+use Shomisha\Crudly\Developers\Crud\PartialDevelopers\LoadDependenciesDeveloper;
+use Shomisha\Crudly\Developers\Crud\Web\Store\FormRequestArgumentDeveloper;
+use Shomisha\Crudly\Managers\BaseDeveloperManager;
+
+class CrudDeveloperManager extends BaseDeveloperManager
+{
+    public function getImplicitBindArgumentDeveloper(): ImplicitBindArgumentsDeveloper
+    {
+        return $this->instantiateDeveloperWithManager(ImplicitBindArgumentsDeveloper::class, $this);
+    }
+
+    public function getFormRequestArgumentDeveloper(): FormRequestArgumentDeveloper
+    {
+        return $this->instantiateDeveloperWithManager(FormRequestArgumentDeveloper::class, $this);
+    }
+
+    public function getInstantiateDeveloper(): InstantiateDeveloper
+    {
+        return $this->instantiateDeveloperWithManager(InstantiateDeveloper::class, $this);
+    }
+
+    public function getLoadDependenciesDeveloper(): LoadDependenciesDeveloper
+    {
+        return $this->instantiateDeveloperWithManager(LoadDependenciesDeveloper::class, $this);
+    }
+}
--- src/Managers/Crud/CrudMethodDeveloperManager.php
+++ src/Managers/Crud/CrudMethodDeveloperManager.php
@@ -3,13 +3,9 @@
 namespace Shomisha\Crudly\Managers\Crud;
 
 use Shomisha\Crudly\Contracts\Developer;
-use Shomisha\Crudly\Developers\Crud\PartialDevelopers\InstantiateDeveloper;
-use Shomisha\Crudly\Developers\Crud\PartialDevelopers\LoadDependenciesDeveloper;
-use Shomisha\Crudly\Developers\Crud\PartialDevelopers\ImplicitBindArgumentsDeveloper;
-use Shomisha\Crudly\Developers\Crud\Web\Store\FormRequestArgumentDeveloper;
 use Shomisha\Crudly\Managers\BaseDeveloperManager;
 
-abstract class CrudMethodDeveloperManager extends BaseDeveloperManager
+abstract class CrudMethodDeveloperManager extends CrudDeveloperManager
 {
     abstract public function getArgumentsDeveloper(): array ;
 
@@ -20,25 +16,4 @@ abstract class CrudMethodDeveloperManager extends BaseDeveloperManager
     abstract public function getMainDeveloper(): Developer;
 
     abstract public function getResponseDeveloper(): Developer;
-
-
-    public function getImplicitBindArgumentDeveloper(): ImplicitBindArgumentsDeveloper
-    {
-        return $this->instantiateDeveloperWithManager(ImplicitBindArgumentsDeveloper::class, $this);
-    }
-
-    public function getFormRequestArgumentDeveloper(): FormRequestArgumentDeveloper
-    {
-        return $this->instantiateDeveloperWithManager(FormRequestArgumentDeveloper::class, $this);
-    }
-
-    public function getInstantiateDeveloper(): InstantiateDeveloper
-    {
-        return $this->instantiateDeveloperWithManager(InstantiateDeveloper::class, $this);
-    }
-
-    public function getLoadDependenciesDeveloper(): LoadDependenciesDeveloper
-    {
-        return $this->instantiateDeveloperWithManager(LoadDependenciesDeveloper::class, $this);
-    }
 }
