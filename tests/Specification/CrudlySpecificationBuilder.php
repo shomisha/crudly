2eb641b1f7006cde34fb9b437539acb90a8e2862
Miša Ković <misa95kovic@gmail.com>
2021-02-09 02:41:21 +0100

    test(*): implement CrudlySpecification and ModelPropertySpecification test builders. WIP

---
+++ tests/Specification/CrudlySpecificationBuilder.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Shomisha\Crudly\Test\Specification;
+
+use Shomisha\Crudly\Data\ModelName;
+use Shomisha\Crudly\Enums\ModelPropertyType;
+use Shomisha\Crudly\Specifications\CrudlySpecification;
+
+class CrudlySpecificationBuilder
+{
+    private string $modelName;
+
+    private ?string $namespace = null;
+
+    private string $rootNamespace = 'App\Models';
+
+    /** @var \Shomisha\Crudly\Test\Specification\PropertySpecificationBuilder[] */
+    private array $properties = [];
+
+    private bool $softDeletion = false;
+    private ?string $softDeletionColumnName = null;
+
+    private bool $timestamps = true;
+
+    private bool $webCrud = true;
+    private bool $webAuthorization = true;
+    private bool $webTests = true;
+
+    private bool $apiCrud = true;
+    private bool $apiAuthorization = true;
+    private bool $apiTests = true;
+
+    public function __construct(string $modelName)
+    {
+        $this->modelName = $modelName;
+    }
+
+    public static function forModel(string $modelName): self
+    {
+        return new self($modelName);
+    }
+
+    public function namespace(?string $namespace = null): self
+    {
+        $this->namespace = $namespace;
+
+        return $this;
+    }
+
+    public function rootNamespace(string $rootNamespace): self
+    {
+        $this->rootNamespace = $rootNamespace;
+
+        return $this;
+    }
+
+    public function property(string $propertyName, ModelPropertyType $propertyType): PropertySpecificationBuilder
+    {
+        $this->properties[$propertyName] = new PropertySpecificationBuilder($propertyName, $propertyType);
+
+        return $this->properties[$propertyName];
+    }
+
+    public function softDeletes(bool $softDeletes = true): self
+    {
+        $this->softDeletion = $softDeletes;
+
+        return $this;
+    }
+
+    public function softDeletionColumn(?string $column): self
+    {
+        $this->softDeletionColumnName = $column;
+
+        return $this;
+    }
+
+    public function timestamps(bool $timestamps = true): self
+    {
+        $this->timestamps = $timestamps;
+
+        return $this;
+    }
+
+    public function webCrud(bool $webCrud = true): self
+    {
+        $this->webCrud = $webCrud;
+
+        return $this;
+    }
+
+    public function webAuthorization(bool $webAuthorization = true): self
+    {
+        $this->webAuthorization = $webAuthorization;
+
+        return $this;
+    }
+
+    public function webTests(bool $webTests = true): self
+    {
+        $this->webTests = $webTests;
+
+        return $this;
+    }
+
+    public function apiCrud(bool $apiCrud = true): self
+    {
+        $this->apiCrud = $apiCrud;
+
+        return $this;
+    }
+
+    public function apiAuthorization(bool $apiAuthorization = true): self
+    {
+        $this->apiAuthorization = $apiAuthorization;
+
+        return $this;
+    }
+
+    public function apiTests(bool $apiTests = true): self
+    {
+        $this->apiTests = $apiTests;
+
+        return $this;
+    }
+
+    public function build(): CrudlySpecification
+    {
+        $model = new ModelName($this->namespace, $this->rootNamespace, $this->namespace);
+
+        return new CrudlySpecification($model, [
+            'properties' => $this->buildProperties(),
+            'has_soft_deletion' => $this->softDeletion,
+            'soft_delete_column_name' => $this->softDeletionColumnName,
+            'has_timestamps' => $this->timestamps,
+            'has_web' => $this->webCrud,
+            'has_web_authorization' => $this->webAuthorization,
+            'has_web_test' => $this->webTests,
+            'has_api' => $this->apiCrud,
+            'has_api_authorization' => $this->apiAuthorization,
+            'has_api_test' => $this->apiTests,
+        ]);
+    }
+
+    /** @return array[] */
+    private function buildProperties(): array
+    {
+        return array_map(function (PropertySpecificationBuilder $propertyBuilder) {
+            return $propertyBuilder->buildArray();
+        }, $this->properties);
+    }
+}
---
+++ tests/Specification/PropertySpecificationBuilder.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace Shomisha\Crudly\Test\Specification;
+
+use Shomisha\Crudly\Enums\ForeignKeyAction;
+use Shomisha\Crudly\Enums\ModelPropertyType;
+use Shomisha\Crudly\Enums\RelationshipType;
+use Shomisha\Crudly\Specifications\ModelPropertySpecification;
+
+class PropertySpecificationBuilder
+{
+    private string $name;
+
+    private ModelPropertyType $type;
+
+    private bool $unsigned = false;
+    private bool $autoIncrement = false;
+
+    private bool $unique = false;
+    private bool $nullable = false;
+
+    private bool $primary = false;
+
+    private bool $foreign = false;
+    private ?string $foreignKeyField = null;
+    private ?string $foreignTable = null;
+    private ?ForeignKeyAction $onUpdate = null;
+    private ?ForeignKeyAction $onDelete = null;
+
+    private bool $hasRelationship = false;
+    private ?string $relationshipName = null;
+    private ?RelationshipType $relationshipType = null;
+
+
+    public function __construct(string $name, ModelPropertyType $type)
+    {
+        $this->name = $name;
+        $this->type = $type;
+    }
+
+    public function unsigned(bool $unsigned = true): self
+    {
+        $this->unsigned = $unsigned;
+
+        return $this;
+    }
+
+    public function autoIncrement(bool $autoIncrement = true): self
+    {
+        $this->autoIncrement = $autoIncrement;
+
+        return $this;
+    }
+
+    public function unique(bool $unique = true): self
+    {
+        $this->unique = $unique;
+
+        return $this;
+    }
+
+    public function nullable(bool $nullable = true): self
+    {
+        $this->nullable = $nullable;
+
+        return $this;
+    }
+
+    public function primary(bool $primary = true): self
+    {
+        $this->primary = $primary;
+
+        return $this;
+    }
+
+    public function isForeign(string $key, string $table, ?ForeignKeyAction $onUpdate, ?ForeignKeyAction $onDelete): self
+    {
+        $this->foreign = true;
+        $this->foreignKeyField = $key;
+        $this->foreignTable = $table;
+        $this->onUpdate = $onUpdate ?? ForeignKeyAction::DO_NOTHING();
+        $this->onDelete = $onDelete?? ForeignKeyAction::DO_NOTHING();
+    }
+
+    public function isNotForeign(): self
+    {
+        $this->foreign = false;
+        $this->foreignKeyField = null;
+        $this->foreignTable = null;
+        $this->onUpdate = null;
+        $this->onDelete = null;
+
+        return $this;
+    }
+
+    public function isRelationship(string $name, RelationshipType $type): self
+    {
+        $this->hasRelationship = true;
+        $this->relationshipName = $name;
+        $this->relationshipType = $type;
+
+        return $this;
+    }
+
+    public function isNotRelationship(): self
+    {
+        $this->hasRelationship = false;
+        $this->relationshipName = null;
+        $this->relationshipType = null;
+
+        return $this;
+    }
+
+    public function buildSpecification(): ModelPropertySpecification
+    {
+        return new ModelPropertySpecification($this->buildArray());
+    }
+
+    public function buildArray(): array
+    {
+        $foreignKeyTarget = [];
+
+        if ($this->foreign) {
+            $foreignKeyTarget = [
+                'table' => $this->foreignTable,
+                'field' => $this->foreignKeyField,
+                'has_relationship' => $this->hasRelationship,
+                'relationship' => [
+                    'name' => $this->relationshipName,
+                    'type' => $this->relationshipType,
+                ],
+                'on_delete' => $this->onDelete,
+                'on_update' => $this->onUpdate,
+            ];
+        }
+
+        return [
+            'name' => $this->name,
+            'type' => $this->type,
+            'is_autoincrement' => $this->autoIncrement,
+            'is_unsigned' => $this->unsigned,
+            'is_nullable' => $this->nullable,
+            'is_unique' => $this->unique,
+            'is_primary' => $this->primary,
+            'is_foreign_key' => $this->foreign,
+            'foreign_key_target' => $foreignKeyTarget,
+        ];
+    }
+}
