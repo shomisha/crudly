9a23bd3a06cab95554005fe2eab92c3b267b7043
Miša Ković <misa95kovic@gmail.com>
2021-02-24 00:52:04 +0100

    fix(tests): fix invalid test namespace

--- tests/Unit/Developers/Partials/FillFieldsSeparatelyDeveloperTest.php
+++ tests/Unit/Developers/Partials/FillFieldsSeparatelyDeveloperTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Shomisha\Crudly\Test\Unit\Developers\WebCrud;
+namespace Shomisha\Crudly\Test\Unit\Developers\Partials;
 
 use Shomisha\Crudly\Config\DeveloperConfig;
 use Shomisha\Crudly\Data\CrudlySet;
