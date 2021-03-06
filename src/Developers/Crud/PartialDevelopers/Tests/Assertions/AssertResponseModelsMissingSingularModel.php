f53b5429cace9a3180b442340556a70f78bfe29f
Miša Ković <misa95kovic@gmail.com>
2021-03-06 18:05:00 +0100

    fix(tests): fix invalid argument order in asserting response models do not contain specified model, fix invalid return type on developer

--- src/Developers/Crud/PartialDevelopers/Tests/Assertions/AssertResponseModelsMissingSingularModel.php
+++ src/Developers/Crud/PartialDevelopers/Tests/Assertions/AssertResponseModelsMissingSingularModel.php
@@ -6,12 +6,13 @@ use Shomisha\Crudly\Contracts\Specification;
 use Shomisha\Crudly\Data\CrudlySet;
 use Shomisha\Crudly\Developers\Tests\TestsDeveloper;
 use Shomisha\Stubless\ImperativeCode\Block;
+use Shomisha\Stubless\ImperativeCode\InvokeMethodBlock;
 use Shomisha\Stubless\References\Reference;
 
 class AssertResponseModelsMissingSingularModel extends TestsDeveloper
 {
     /** @param \Shomisha\Crudly\Specifications\CrudlySpecification $specification */
-    public function develop(Specification $specification, CrudlySet $developedSet): Block
+    public function develop(Specification $specification, CrudlySet $developedSet): InvokeMethodBlock
     {
         $model = $specification->getModel();
         $modelVar = Reference::variable(
@@ -21,18 +22,16 @@ class AssertResponseModelsMissingSingularModel extends TestsDeveloper
             "response" . ucfirst($this->guessSingularModelVariableName($model)) . 'Ids'
         );
 
-        return Block::fromArray([
-            Block::invokeMethod(
+        return Block::invokeMethod(
                 Reference::this(),
                 'assertNotContains',
                 [
-                    $modelIdsVar,
                     Reference::objectProperty(
                         $modelVar,
                         $specification->getPrimaryKey()->getName()
-                    )
+                    ),
+                    $modelIdsVar,
                 ]
-            )
-        ]);
+            );
     }
 }
