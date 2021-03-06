597799f5b00c0f57eb5a8914504f3c506bed9442
Miša Ković <misa95kovic@gmail.com>
2021-03-06 18:54:13 +0100

    refactor(tests): add refresh model to asserting soft deletion column is not null

--- src/Developers/Crud/PartialDevelopers/Tests/Assertions/AssertModelDeletedDeveloper.php
+++ src/Developers/Crud/PartialDevelopers/Tests/Assertions/AssertModelDeletedDeveloper.php
@@ -6,12 +6,13 @@ use Shomisha\Crudly\Contracts\Specification;
 use Shomisha\Crudly\Data\CrudlySet;
 use Shomisha\Crudly\Developers\Tests\TestsDeveloper;
 use Shomisha\Crudly\Specifications\CrudlySpecification;
-use Shomisha\Stubless\ImperativeCode\InvokeBlock;
+use Shomisha\Stubless\Contracts\Code;
+use Shomisha\Stubless\ImperativeCode\Block;
 
 class AssertModelDeletedDeveloper extends TestsDeveloper
 {
     /** @param \Shomisha\Crudly\Specifications\CrudlySpecification $specification */
-    public function develop(Specification $specification, CrudlySet $developedSet): InvokeBlock
+    public function develop(Specification $specification, CrudlySet $developedSet): Code
     {
         if ($specification->hasSoftDeletion()) {
             return $this->assertSoftDeleted($specification, $developedSet);
@@ -20,12 +21,15 @@ class AssertModelDeletedDeveloper extends TestsDeveloper
         return $this->assertHardDeleted($specification, $developedSet);
     }
 
-    protected function assertSoftDeleted(CrudlySpecification $specification, CrudlySet $developedSet): InvokeBlock
+    protected function assertSoftDeleted(CrudlySpecification $specification, CrudlySet $developedSet): Block
     {
-        return $this->getManager()->getAssertSoftDeletedColumnIsNotNullDeveloper()->develop($specification, $developedSet);
+        return Block::fromArray([
+            $this->getManager()->getRefreshModelDeveloper()->develop($specification, $developedSet),
+            $this->getManager()->getAssertSoftDeletedColumnIsNotNullDeveloper()->develop($specification, $developedSet)
+        ]);
     }
 
-    protected function assertHardDeleted(CrudlySpecification $specification, CrudlySet $developedSet): InvokeBlock
+    protected function assertHardDeleted(CrudlySpecification $specification, CrudlySet $developedSet): Code
     {
         return $this->getManager()->getAssertDatabaseMissingModelDeveloper()->develop($specification ,$developedSet);
     }
